b09a8bca9aa3edd2a2490b7a166800038f0d904e
Axel Díaz <diaz.axelio@gmail.com>
2013-04-14 12:00:03 -0430

    Probando uso directo de la página inicial al admin

--- umail/urls.py
+++ umail/urls.py
@@ -10,4 +10,5 @@ urlpatterns = patterns('',
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^django_messages/', include('django_messages.urls')),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^/', include(admin.site.urls)),
 )

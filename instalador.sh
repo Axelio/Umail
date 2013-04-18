db47cfce12a9f5e2f8aba8657e28cbb66d12b914
Axel Díaz <diaz.axelio@gmail.com>
2013-04-18 10:24:39 -0430

    Mejorando instalador con uso del requirements.txt

--- instalador.sh
+++ instalador.sh
@@ -3,9 +3,9 @@
 
 exec su -c "
 
-echo 'Iniciando la instalación de python pip';
-aptitude install python-pip;
+echo 'Iniciando la instalación de paquetes';
+aptitude install python-pip gitk tig gource postgresql;
 
-echo 'Iniciando la instalación y actualización de Django, visores de git, docutils, visores de progreso animado de git, psycopg2 para soporte a postgres, motor de base de datos PostgreSQL';
-pip install django; pip install --upgrade django django-extensions; aptitude install gitk tig gource python-psycopg2 postgresql python-docutils;
+echo 'Iniciando la instalación de aplicaciones python';
+pip install -r requirements.txt
 "

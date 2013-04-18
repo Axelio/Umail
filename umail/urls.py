73c512c238f65686394585a2d569019be319919b
Axel Díaz <diaz.axelio@gmail.com>
2013-04-18 09:23:25 -0430

    Editando configuraciones de umail

--- umail/urls.py
+++ umail/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import patterns, include, url
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 from django.conf import settings
+from django.views.generic import TemplateView
 
 admin.autodiscover()
 
@@ -13,7 +14,7 @@ urlpatterns = patterns('',
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^django_messages/', include('django_messages.urls')),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', include(admin.site.urls)),
+    url(r'^$', TemplateView.as_view(template_name="user/index.html")),
 )
 if not settings.DEBUG:
     urlpatterns += patterns('',

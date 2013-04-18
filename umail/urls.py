31959cb90df81c5b67d7186142b06ee6456ab7f1
Axel Díaz <diaz.axelio@gmail.com>
2013-04-18 12:01:52 -0430

    Cambiando url para la página inicial del proyecto

--- umail/urls.py
+++ umail/urls.py
@@ -14,7 +14,7 @@ urlpatterns = patterns('',
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^django_messages/', include('django_messages.urls')),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', TemplateView.as_view(template_name="user/index.html")),
+    url(r'^$','auth.views.index'),
 )
 if not settings.DEBUG:
     urlpatterns += patterns('',

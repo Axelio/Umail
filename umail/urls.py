7880c87c4402ebdaac9e7b486cd4ce12f1420f6b
Axel Díaz <diaz.axelio@gmail.com>
2013-04-19 17:42:51 -0430

    Añadiendo url para inbox

--- umail/urls.py
+++ umail/urls.py
@@ -18,6 +18,7 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'logout$',logout,{'next_page':'/'},),
     url(r'^$','auth.views.index'),
+    url(r'^inbox/$', 'django_messages.views.inbox'),
 )
 if not settings.DEBUG:
     urlpatterns += patterns('',

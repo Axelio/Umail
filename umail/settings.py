3f032889b212187ce1b2ece45621320ffab62f5b
Axel Díaz <diaz.axelio@gmail.com>
2013-04-21 13:52:24 -0430

    Añadiendo Django Debug Toolbar

--- umail/settings.py
+++ umail/settings.py
@@ -101,9 +101,13 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
     # Uncomment the next line for simple clickjacking protection:
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
+
+INTERCEPT_REDIRECTS = False
+INTERNAL_IPS = ('127.0.0.1',)
 
 ROOT_URLCONF = 'umail.urls'
 
@@ -150,6 +154,18 @@ COMPRESS_JS = {
     },
 }
 
+DEBUG_TOOLBAR_PANELS = (
+    'debug_toolbar.panels.version.VersionDebugPanel',
+    'debug_toolbar.panels.timer.TimerDebugPanel',
+    'debug_toolbar.panels.settings_vars.SettingsVarsDebugPanel',
+    'debug_toolbar.panels.headers.HeaderDebugPanel',
+    'debug_toolbar.panels.request_vars.RequestVarsDebugPanel',
+    'debug_toolbar.panels.template.TemplateDebugPanel',
+    'debug_toolbar.panels.sql.SQLDebugPanel',
+    'debug_toolbar.panels.signals.SignalDebugPanel',
+    'debug_toolbar.panels.logger.LoggingPanel',
+)
+
 INSTALLED_APPS = (
     'auth',
     'django.contrib.auth',
@@ -162,6 +178,7 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     'django_messages',
     'django_extensions',
+    'debug_toolbar',
     'ajax_select',
     'grappelli',
 	'personas',

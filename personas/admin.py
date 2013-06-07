af3c5e0980c5fabd993e137298070cff2e6bf765
Axel Díaz <diaz.axelio@gmail.com>
2013-06-07 23:00:49 +0000

    Añadiendo filtro vertical para cargos autorizados

--- personas/admin.py
+++ personas/admin.py
@@ -5,8 +5,9 @@ from django.contrib import admin
 from personas.models import *
 
 class PersonasAdmin(admin.ModelAdmin):
-    search_fields   =['num_identificacion','primer_nombre','primer_apellido','email','telefono', 'cargo_principal','cargos_autorizados']
+    search_fields   = ['num_identificacion','primer_nombre','primer_apellido','email','telefono', 'cargo_principal','cargos_autorizados']
     list_display    = ['num_identificacion','primer_nombre','segundo_nombre','primer_apellido','segundo_apellido','genero','email','telefono']
+    filter_vertical = ['cargos_autorizados']
 admin.site.register(Personas, PersonasAdmin)
 
 class PersonalAdmin(admin.ModelAdmin):

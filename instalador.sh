1d2e39bf2c243823833fa4848e8dded7f7472c33
Jennifer Montilla <montilla.jennifer@gmail.com>
2013-07-17 08:09:03 +0000

    sacando PIL del super usuario

--- instalador.sh
+++ instalador.sh
@@ -6,12 +6,13 @@ exec su -c "
 echo 'Iniciando la instalación de paquetes';
 aptitude install python-pip gitk tig gource postgresql python-reportlab python-psycopg2;
 
+echo 'Iniciando la instalación de aplicaciones python';
+pip install -r requirements.txt
+"
+
 echo 'Iniciando la instalación de PIL';
 wget http://effbot.org/downloads/Imaging-1.1.7.tar.gz;
 tar xvzf Imaging-1.1.7.tar.gz;
 python Imaging-1.1.7/setup.py install;
 rm -r Imagin*;
 
-echo 'Iniciando la instalación de aplicaciones python';
-pip install -r requirements.txt
-"

202eec48f525ee3d8aa6e95d385e40d3c58382e7
Jennifer Montilla <montilla.jennifer@gmail.com>
2013-07-19 07:20:15 +0000

    Mejorando estilo visual de las noticias

--- noticias/urls.py
+++ noticias/urls.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from django.conf.urls.defaults import *
 urlpatterns=patterns('',
-    (r'^(\d+)$','noticias.views.detalle_noticia'),
+    url(r'^(\d+)$','noticias.views.detalle_noticia', name='detalle_noticia'),
     (r'^.*$', 'noticias.views.todas_noticias'), #Vista por defecto
 )

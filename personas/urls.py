78b2113343ef1307fdb69abb3467f56d1092d56a
Axel Díaz <diaz.axelio@gmail.com>
2013-09-07 09:09:00 -0430

    Completando funcionamiento de asignación y cambio de contraseñas

--- personas/urls.py
+++ personas/urls.py
@@ -5,7 +5,6 @@ from django.contrib.auth.forms import PasswordChangeForm
 
 urlpatterns=patterns('',
     url(r'^preguntas_secretas/(?P<user_id>[\d]+)/$', Preguntas_Secretas.as_view(), name='preguntas_secretas'),
+    url(r'^(?P<palabra_clave>\w+)_clave.*$', 'personas.views.password_change', name='cambiar_clave'),
     url(r'^.*$', Perfil.as_view(), name='perfil'),
-
-
 )
--- personas/views.py
+++ personas/views.py
@@ -6,7 +6,7 @@ from django.http import Http404
 from django.shortcuts import render_to_response, get_object_or_404
 from django.contrib.auth.models import User
 from django.template import RequestContext
-from django.contrib.auth.forms import PasswordChangeForm
+from django.contrib.auth.forms import PasswordChangeForm, SetPasswordForm
 from django.views.decorators.csrf import csrf_protect
 from django.core.context_processors import csrf
 from django import forms
@@ -141,48 +141,39 @@ class Perfil(View):
                             form = form,
                         )
 
-perfil = login_required(Perfil)
-def perfil(request, template_name='usuario/perfil/perfil.html', mensaje=''):
-    c = {}
-    c.update(csrf(request))
-    c.update({'request':request})
-  
-    persona = PerfilForm(instance=request.user.profile.persona)
-    if request.method == 'POST':
-        form = PerfilForm(request.POST)
-        if form.is_valid():
-            # Para editar los datos se le pasa el modelform  y se instancia con el request.profile.persona para guardar los cambios realizados
-            form = PerfilForm(request.POST, instance=request.user.profile.persona)
-            form.save()
-            mensaje = 'Datos guardados exitosamente'
-    c.update({
-        'mensaje':mensaje,
-        'persona':persona,
-        })
-    return render_to_response(template_name, c)
-
 @csrf_protect
 def password_change(request,
-                    template_name='user/personas/cambiar_clave.html',
-                    post_change_redirect='user/personas/perfil.html',
-                    password_change_form=PasswordChangeForm,
-                    current_app=None, extra_context=None):
-    if request.method == "POST":
-        form = password_change_form(user=request.user, data=request.POST)
-        if form.is_valid():
-            form.save()
-            mensaje = u'Contraseña cambiada exitosamente'
-            form = password_change_form(user=request.user)
-            context.update({'form':form})
-            context.update({'request':request})
-            context.update({'mensaje':mensaje})
-            return render_to_response(post_change_redirect, context)
+                    template_name='usuario/perfil/cambiar_clave.html',
+                    current_app=None, extra_context=None, expresion=None, tipo_mensaje=None, mensaje=None, palabra_clave=None):
+    context = {}
+    context.update(csrf(request))
+
+    # Determinar cuál formulario usar.
+    # 'Asignar' no necesita contraseña anterior
+    # 'Cambiar' necesita contraseña anterior
+    accion = ''
+    if palabra_clave == 'asignar':
+        form = SetPasswordForm(user=request.user, data=request.POST or None)
+        accion = 'cambiada'
+    elif palabra_clave == 'cambiar':
+        accion = 'actualizada'
+        form = PasswordChangeForm(user=request.user, data=request.POST or None)
+
+    if form.is_valid():
+        form.save()
+        mensaje = u'Contraseña %s exitosamente' %(accion)
+        tipo_mensaje = 'success'
     else:
-        form = password_change_form(user=request.user)
-        context = {
-            'form': form,
-            'request':request
-        }
+        tipo_mensaje = 'error'
+        for error in form.errors:
+            mensaje = form.errors['%s' %(error)].as_text().replace('* ','')
+
+    (tipo_mensaje, expresion) = msj_expresion(tipo_mensaje)
+    context.update({'formulario':form})
+    context.update({'request':request})
+    context.update({'mensaje':mensaje})
+    context.update({'tipo_mensaje':tipo_mensaje})
+    context.update({'expresion':expresion})
     return render_to_response(template_name, context)
 password_change = login_required(password_change)
 
@@ -272,27 +263,4 @@ class Preguntas_Secretas(View):
                     usuario = User.objects.get(id=kwargs['user_id'])
                 usuario.backend = 'django.contrib.auth.backends.ModelBackend'
                 login(request, usuario)
-                return HttpResponseRedirect('/perfil/')
-
-
-        #return HttpResponseRedirect('/invalido')
-        '''
-        if form.is_valid():
-            print "Todo bien"
-            return renderizar_plantilla(request, 
-                                plantilla=self.template, 
-                                tipo_mensaje = self.tipo_mensaje, 
-                                expresion = self.expresion, 
-                                mensaje = self.mensaje, 
-                                form = form
-                            )
-        else:
-            return renderizar_plantilla(request, 
-                                plantilla=self.template, 
-                                tipo_mensaje = self.tipo_mensaje, 
-                                expresion = self.expresion, 
-                                mensaje = self.mensaje, 
-                                form = form
-                            )
-            '''
-
+                return HttpResponseRedirect('/perfil/asignar_clave/')

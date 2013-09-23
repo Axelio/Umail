7b2cf4a0c90e673431031ef74217f6b0b86f9d23
Jennifer Montilla <montilla.jennifer@gmail.com>
2013-09-23 10:14:40 -0430

    actualizando código

--- reportes/forms.py
+++ reportes/forms.py
@@ -41,10 +41,12 @@ class ConsultaMemoForm(forms.ModelForm):
 class Feedback_Form(forms.ModelForm):
     class Meta:
         model = Comentarios
+        texto= u"Escriba aquí los detalles."
+        texto.join(u'\nPor favor sea lo más específico posible.')
         widgets = {
         'sentimiento': forms.Select(attrs={'class':'choices', 'value':''}),
-        'pregunta': forms.TextInput(attrs={'class':'text', 'required':'required', 'value':'','placeholder':u'Pregunta', 'style':'width:98%'}),
-        'comentario': forms.Textarea(attrs={'class':'text', 'required':'required', 'value':'','placeholder':u'Introduzca su comentario',  'style':'width:98%'}),
-        'nombre': forms.TextInput(attrs={'class':'text', 'required':'required', 'value':'','placeholder':u'¿Cómo se llama?', 'style':'width:98%'}),
-        'correo': forms.TextInput(attrs={'class':'email', 'required':'required', 'value':'','placeholder':u'Correo electrónico', 'style':'width:98%'}),
+        'pregunta': forms.TextInput(attrs={'class':'prependedInput', 'required':'required', 'value':'','placeholder':u'breve descripción', 'size':16}),
+        'comentario': forms.Textarea(attrs={'class':'text', 'required':'required', 'value':'','placeholder':texto,  'style':'width:230px; height:230px'  }),
+        'nombre': forms.TextInput(attrs={'class':'prependedInput', 'required':'required', 'value':'', 'placeholder':u'nombre apellido', 'size':16}),
+        'correo': forms.TextInput(attrs={'class':'prependedInput', 'required':'required', 'value':'', 'type':'email', 'placeholder':u'usuario@ejemplo.com', 'size':16}),
         }

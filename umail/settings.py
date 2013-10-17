08c59bdfe91c8e8ace41c806491a9c11af19de76
Jennifer Montilla <montilla.jennifer@gmail.com>
2013-10-16 21:24:34 -0430

    actualizando código

--- umail/settings.py
+++ umail/settings.py
@@ -42,7 +42,7 @@ DATABASES = {
         'NAME': 'db_umail',
         'USER': 'umail',                      # Not used with sqlite3.
         'PASSWORD': 'umail86245',                  # Not used with sqlite3.
-        'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
+        'HOST': '192.168.1.5',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
